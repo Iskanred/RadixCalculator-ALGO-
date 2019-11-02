6da1ef573e0a4a82894443df77f6496862eab88e
Iskander <nafikov.iskander@mail.ru>
2019-11-02 16:17:30 +0500

    Added: - Edited: Typos and mistakes
    Deleted:
    -

--- src/com/iskandev/rdxcalc/exceptions/TooLargeNumberException.java
+++
@@ -1,6 +0,0 @@
-package com.iskandev.rdxcalc.exceptions;
-
-public final class TooLargeNumberException extends Exception {
-
-    public TooLargeNumberException() { super("!TOO LARGE NUMBER!"); }
-}
